eec632043ee85cf41a3ebd2e5eaaeb7ac0a1cb0a
jeremysampl <81999069+jeremysampl@users.noreply.github.com>
2023-10-11 13:09:01 -0400

    Rephrased and fixed certain sentences

--- src/components/Pages/ExperiencePage.js
+++ src/components/Pages/ExperiencePage.js
@@ -22,21 +22,21 @@ export default function ExperiencePage() {
                 <div style={{textAlign: 'left'}}>
                     <h3 style={{fontSize: isMobile ? 20 : 24}}>During my 4-month co-op at The Watering Can, I had the opportunity to work on many exciting projects with some extremely talented individuals! Here is a summary of my time at the company:</h3>
                     <ul style={{margin: '10px 10px 10px 30px', fontSize: isMobile ? 18 : 22, gap: 10, display: 'flex', flexDirection: 'column'}}>
-                        <li>Lead the development of a new point of sale (POS) system for their café.
+                        <li>Led the development of a new point of sale (POS) system for their café.
                             <ul style={{marginLeft: 30}}>
                                 <li>Developed a new interactive map that displays table statuses and links tables to their orders.</li>
                                 <li>Used WebSocket connections to ensure the data is shown in real-time on all devices simultaneously.</li>
                                 <li>Created APIs to handle all requests, fetching and storing any data necessary.</li>
                             </ul>
                         </li>
-                        <li>Lead the development of a new chits app for ordered food and drinks.
+                        <li>Led the development of a new chits application for ordered food and drinks.
                             <ul style={{marginLeft: 30}}>
-                                <li>Replaced an outdated system with a new lightweight React app, offering significantly improved integration with their pre-existing systems.</li>
-                                <li>Displays and controls incoming chits, printing them to the desired location when ready.</li>
-                                <li>Offers significant integration with the POS system, such as almost instantaneous chit receival due to seamless connections between all devices.</li>
+                                <li>Replaced an outdated system with a new lightweight React application, offering significantly improved integration with their pre-existing systems.</li>
+                                <li>Produced a system that displays and controls incoming chits, printing them to the desired location when ready.</li>
+                                <li>Created seamless data syncronization between all devices, permitting multiple instances of the program to run simultaneously.</li>
                             </ul>
                         </li>
-                        <li>Created WordPress plugins to add new features to their website, as well as update existing ones.</li>
+                        <li>Developed WordPress plugins to add new features to their website, as well as update existing ones.</li>
                         <li>Used languages, libraries and frameworks such as PHP, JavaScript, jQuery, React and MySQL.</li>
                     </ul>
                 </div>
