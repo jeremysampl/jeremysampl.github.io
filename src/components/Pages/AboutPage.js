977c3312029d4d28b698754347243576982ce185
jeremysampl <jeremysampl@live.com>
2023-12-07 11:07:24 -0500

    Updated about page to reflect second year

--- src/components/Pages/AboutPage.js
+++ src/components/Pages/AboutPage.js
@@ -19,10 +19,9 @@ export default function AboutPage() {
                 <div className="about-col">
                     <div className="about-col-margin">
                         <h3>
-                            Hello there! My name is Jeremy Sampl and I am a current undergraduate computer science student at McMaster University.<br/><br/>
+                            Hello there! My name is Jeremy Sampl and I am a current undergraduate computer science student at McMaster University in Hamilton, Ontario, Canada.<br/><br/>
                             I'd consider myself to be an extremely motivated individual with a passion for coding and learning.
-                            As I attend my first year, I am very excited to start my career as a professional developer and acquire an abundance of new
-                            knowledge through academic learning and first-hand experience.<br/><br/>
+                            This can be easily seen through my impressive 4.0 GPA and my many personal projects, which I highly encourage you to check out!<br/><br/>
                             I hope to be able to help push computing to its limits in order to further improve user experience and the overall usefulness
                             of computers in an ever-increasing tech future.
                         </h3>
