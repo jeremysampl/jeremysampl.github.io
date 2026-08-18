adead394221b350787b049e82d32d6b0961fbb24
Jeremy Sampl <jeremysampl@live.com>
2026-08-18 13:05:32 -0400

    Added more info to Geckode

--- src/components/pages/projects/GeckodePage.tsx
+++ src/components/pages/projects/GeckodePage.tsx
@@ -95,45 +95,93 @@ const technologies: TechnologyItem[] = [
 	
 ];
 
-const galleryStrip: GalleryItem[] = [
+const platformerGame: GalleryItem = {
+	title: 'Platformer game',
+	path: 'geckode/platformer-game.png',
+	description: 'A simple platformer game built with Geckode.',
+};
+
+const loginStrip: GalleryItem[] = [
 	{
-		title: 'Platformer game',
-		path: 'geckode/platformer-game.png',
-		description: 'A simple platformer game built with Geckode.',
+		title: 'Register',
+		path: 'geckode/register.png',
+		description: 'The form to register an account with Geckode.',
 	},
+	{
+		title: 'Login',
+		path: 'geckode/login.png',
+		description: 'The form to login to an existing Geckode account.',
+	},
+];
+
+const projectsStrip: GalleryItem[] = [
+	{
+		title: 'Projects table',
+		path: 'geckode/projects-table.png',
+		description: 'The projects page allows you to manage your projects and view other users\' projects.',
+	},
+	{
+		title: 'Project filtering',
+		path: 'geckode/filter-projects.png',
+		description: 'Fine-tuned filters allow users to quickly find the projects they are looking for or discover new ones.',
+	},
+	{
+		title: 'Project creation',
+		path: 'geckode/create-project.png',
+		description: 'The project creation modal allows you to create a new project with a name and thumbnail.',
+	},
+	{
+		title: 'Project details',
+		path: 'geckode/project-details.png',
+		description: 'View and modify project details all in one place.'
+	}
 ];
 
 const editorsStrip: GalleryItem[] = [
     {
         title: 'Sprite editor',
         path: 'geckode/sprite-editor.png',
-        description: 'The sprite editor allows you to create and edit sprites for your projects.',
+        description: 'The sprite editor allows users to create and edit sprites for their projects.',
     },
     {
         title: 'Tilemap editor',
         path: 'geckode/tilemap-editor.png',
-        description: 'The tilemap editor allows you to create and edit the worlds in your games.',
+        description: 'The tilemap editor allows users to create and edit the worlds in their games.',
     },
     {
         title: 'Tileset editor',
         path: 'geckode/tileset-editor.png',
-        description: 'The tileset editor allows you to create and edit the tilesets to speed up and organize tilemap editing.',
+        description: 'The tileset editor allows users to create and edit the tilesets to speed up and organize tilemap editing.',
     },
     {
         title: 'Tile editor',
         path: 'geckode/tile-editor.png',
-        description: 'The tile editor allows you to create and edit the tiles for your tilesets.',
+        description: 'The tile editor allows users to create and edit the tiles for their tilesets.',
     },
 ];
 
 const shareStrip: GalleryItem[] = [
     {
-        title: 'Project sharing',
+        title: 'User sharing',
         path: 'geckode/share-project-users.png',
-        description: 'You can share your projects with other users, allowing them to edit or simply view your projects.',
+        description: 'Projects can be shared with other users, allowing them to edit or simply view your projects.',
     },
-    // TODO: Add project sharing with organizations
-    // TODO: Add public game sharing
+	{
+		title: 'Organization sharing',
+		path: 'geckode/share-project-organizations.png',
+		description: `Projects can be shared with organizations, providing an easy way to give the same access to a specific group of users,
+			such as a classroom or team.`,
+	},
+	{
+		title: 'Link sharing',
+		path: 'geckode/share-project-links.png',
+		description: 'Projects can be shared with a link, allowing anyone with the link to play the game without needing to create an account.',
+	},
+	{
+		title: 'Shared game',
+		path: 'geckode/shared-game.png',
+		description: 'A game being played from a shared link. This also provides the ability for games to be versioned.'
+	},
 ];
 
 const organizationsStrip: GalleryItem[] = [
@@ -154,6 +202,12 @@ const howItWorksStrip: GalleryItem[] = [
     },
 ];
 
+const demoVideo: GalleryItem = {
+	title: 'Demo video',
+	path: 'geckode/geckode-demo.mp4',
+	description: 'The original demo video of Geckode showcased at the end of the capstone course.',
+};
+
 export default function GeckodePage() {
 	const project = getProject('geckode');
 
@@ -165,22 +219,53 @@ export default function GeckodePage() {
 				title: project.title,
 				description: (
 					<>
-						<p className="project-card__intro">
+						<h2>Overview</h2>
+						<p>
                             Geckode is a web-based block coding platform designed to make programming intuitive for
                             beginners while providing powerful tools for experienced developers. Unique features such
                             as a true physics engine, real-time collaboration, and new blocks that condense complex game
                             logic into a single block give Geckode significant advantages over platforms like Scratch and
                             MakeCode.
 						</p>
-                        <h3>Customization</h3>
-						<ul className="project-card__points">
-							<li>
-								There is plenty of customization available through editors:
-								<ProjectFilmstrip media={editorsStrip} />
-							</li>
-						</ul>
+
+						<h2>Accounts</h2>
+
+						<h3>Register/Login</h3>
+						<p>
+							Prospective users can easily create an account using an email or a Google account. Existing users
+							can use the same method to login:
+						</p>
+						<ProjectFilmstrip media={loginStrip} aspectRatio={[3, 3.6]} />
+						
+						<h3>Project Management</h3>
+						<p>
+							Users have access to projects, in which they can create, manage, and view them straight from the projects page:
+						</p>
+						<ProjectFilmstrip media={projectsStrip} aspectRatio={[3, 4]} maxPerRow={2} />
+
+						<h2>Projects</h2>
+
+						<h3>Editor</h3>
+						<p>
+							Projects can be developed using the editor, which provides a suite of tools for creating and editing games:
+						</p>
+						<ProjectFilmstrip media={[platformerGame, ...editorsStrip]} />
+
+						<h3>Sharing</h3>
+						<p>
+							Projects can be shared with other users, allowing them to edit or simply view your projects. Sharing can be done
+							with individual users, such as friends or colleagues, or with organizations, such as a classroom or team:
+						</p>
+						<ProjectFilmstrip media={shareStrip} maxPerRow={2} />
+
+						<h3>Organizations</h3>
+						<p>
+							Organizations can be used to quickly create, manage and share projects among a group of users, such as a classroom
+							or team:
+						</p>
+						<ProjectFilmstrip media={organizationsStrip} />
 					</>
-                    // TODO: Add more information about Geckode's features and how it works
+                    // TODO: Add details about the physics engine, the real-time collaboration, and how they work
 				),
 			}}
 			overview={{
@@ -205,7 +290,7 @@ export default function GeckodePage() {
 			technologies={technologies}
 			siteIconSrc="/images/projects/geckode/geckode-icon.png"
 			siteIconAlt="Geckode"
-			gallery={[...galleryStrip, ...organizationsStrip, ...editorsStrip, ...shareStrip, ...howItWorksStrip]}
+			gallery={[platformerGame, ...editorsStrip, ...shareStrip, ...organizationsStrip, ...howItWorksStrip, demoVideo, ...projectsStrip, ...loginStrip]}
 		/>
 	);
 }
